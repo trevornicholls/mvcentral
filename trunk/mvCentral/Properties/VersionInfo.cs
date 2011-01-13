51c027b9cb67523e80ee428ab2eeb237f8d2c34d
gupaudio@hotmail.com <gupaudio@hotmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-01-13 08:20:14 +0000

    forgot the playlist skin file

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.27")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.27")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.28")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.28")]
