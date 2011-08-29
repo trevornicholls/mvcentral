2124b576efb14bdfdd031465578c156eec1418f1
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-08-29 11:08:22 +0000

    Updated externals to 1.2RC and removed old beta externals

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.39")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.39")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.41")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.41")]
