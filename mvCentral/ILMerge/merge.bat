3a096a7afe13f959c9434915e2ba65c1bce695a6
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-09-08 18:49:04 +0000

    Missing files from last commit

---
+++ mvCentral/ILMerge/merge.bat
@@ -0,0 +1,4 @@
+@echo off
+IF EXIST mvCentral_UNMERGED.dll del mvCentral_UNMERGED.dll
+ren mvCentral.dll mvCentral_UNMERGED.dll
+ilmerge /out:mvCentral.dll mvCentral_UNMERGED.dll nlog.dll cornerstone.dll
