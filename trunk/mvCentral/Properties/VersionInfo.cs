ab319410027e0b12b7770a62ad5fdbdc1361a945
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-09-08 18:49:04 +0000

    Missing files from last commit

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.55")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.55")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.57")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.57")]
