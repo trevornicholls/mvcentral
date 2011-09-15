77b1cd461e3f36b73a8b36d9e7329ff2809a1d14
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-09-15 07:55:09 +0000

    Smart Playlist newest video option now uses setting - default to 7 days

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.65")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.65")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.66")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.66")]
