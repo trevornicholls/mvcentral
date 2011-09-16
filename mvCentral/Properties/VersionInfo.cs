5276e096788df2e55dc033e9064b56c4bfb36be3
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-09-16 00:48:16 +0000

    Change skin id for mvCentral.Playlist.xml to 112012 to be consistent

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.67")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.67")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.68")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.68")]
