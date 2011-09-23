120a85b0e0f61189a48e5b3f58dcd192e106a7a1
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-09-23 23:00:06 +0000

    Small tweaks

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.94")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.94")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.95")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.95")]
