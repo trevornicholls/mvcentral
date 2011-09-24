0dd9adbef70483863460f5fe64cf130c28bef0b2
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-09-24 21:59:43 +0000

    New defaultSrtist image and finished off StreamedMP Skin

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.95")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.95")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.96")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.96")]
