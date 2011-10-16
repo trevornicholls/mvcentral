72b69df4f7d5794c370ad1e54db02e3ae7aaeb81
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-10-16 21:02:39 +0000

    Overlap fixes

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.145")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.145")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.146")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.146")]
