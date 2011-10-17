8300f6620e6a2aed2ff120322317acfeec540444
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-10-17 22:48:14 +0000

    Fix spellings. Fixed Coverflow support in Playlists and updated skins tweaked the way #mvCentral.Hierachy works. If on Track will be set to Artist Name | Album Name (Album Name will not be added if no album) If on Album will be set Artist Name Added #itemtype property Thx to Dadeo for point out above.

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.148")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.148")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.149")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.149")]
