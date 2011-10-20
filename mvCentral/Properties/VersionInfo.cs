3c891330e93a122e54f4264c55fbcd320c581f54
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-10-20 17:45:48 +0000

    Add missing file

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.156")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.156")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.157")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.157")]
