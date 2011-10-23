548b80befd6b8b7ff95add3e5bb0eaddafd77873
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-10-23 14:01:14 +0000

    Fix issue 20

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.163")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.163")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.164")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.164")]
