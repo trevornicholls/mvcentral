4a408c8bb61b6a93f7274e18d01fb8843024a79e
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-10-29 19:45:28 +0000

    Added blank DB warning to GUI

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.174")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.174")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.175")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.175")]
