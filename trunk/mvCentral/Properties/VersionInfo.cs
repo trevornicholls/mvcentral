abb9cd0f7def3efe52b31aef0b57cdfe21dc1dcd
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-10-30 22:45:37 +0000

    Alpha sort the Configure Grnres multiselect dialog

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.177")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.177")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.178")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.178")]
