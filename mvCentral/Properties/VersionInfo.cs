5468519fa9b39b1f9065797793eb39a3befd236a
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-11-01 08:10:14 +0000

    Further fix to Issue 25 - Now check DB is valid and if not reset to artist and store Loading display in config now displays counts of artists, albums and videos processed.

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.179")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.179")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.180")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.180")]
