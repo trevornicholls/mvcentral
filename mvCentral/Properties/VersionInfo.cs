2c08aab46a4dd4351429dea7bf8904f75a26a088
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-11-06 18:44:29 +0000

    Fix issue where different track from selected was played.

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.186")]
-[assembly: System.Reflection.AssemblyFileVersion("0.5.0.186")]
+﻿[assembly: System.Reflection.AssemblyVersion("0.5.0.187")]
+[assembly: System.Reflection.AssemblyFileVersion("0.5.0.187")]
