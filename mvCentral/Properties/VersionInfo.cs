208d6d8e0573d2a1cac6e54cc83e430c0ef912a7
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-11-18 11:20:39 +0000

    Added Hover Images (needs some work on the gfx)

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.207")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.207")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.208")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.208")]
