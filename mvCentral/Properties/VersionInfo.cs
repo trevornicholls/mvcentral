ca91f4ba20ace9bfbb313d594faf5d6849c4d006
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-11-19 22:56:20 +0000

    MPEI update.xml..sorted

--- mvCentral/Properties/VersionInfo.cs
+++ mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.228")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.228")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.229")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.229")]
