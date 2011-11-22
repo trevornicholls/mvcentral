56170f84c1a310570e7565eda284b52b16775cd9
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-11-22 18:55:54 +0000

    Update to Release 1.0.0.237

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.236")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.236")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.237")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.237")]
