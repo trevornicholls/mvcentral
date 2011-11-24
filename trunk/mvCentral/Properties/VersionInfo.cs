92ee43de37d101f7a037d80725f14e3eb2cc85ec
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2011-11-24 09:11:18 +0000

    Disable Album support option now respected in GUI as well as Import

--- trunk/mvCentral/Properties/VersionInfo.cs
+++ trunk/mvCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.239")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.239")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.0.0.240")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.240")]
