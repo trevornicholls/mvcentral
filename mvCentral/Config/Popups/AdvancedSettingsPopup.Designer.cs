ea9b2a591aca47542285037e49a0e47c9a8ac201
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2012-02-26 11:44:46 +0000

    Centre Advanced Settings warning/panel

--- mvCentral/Config/Popups/AdvancedSettingsPopup.Designer.cs
+++ mvCentral/Config/Popups/AdvancedSettingsPopup.Designer.cs
@@ -23,31 +23,32 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.advancedSettingsWarningPane1 = new  AdvancedSettingsWarningPane();
-            this.SuspendLayout();
-            // 
-            // advancedSettingsWarningPane1
-            // 
-            this.advancedSettingsWarningPane1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.advancedSettingsWarningPane1.Location = new System.Drawing.Point(0, 0);
-            this.advancedSettingsWarningPane1.Name = "advancedSettingsWarningPane1";
-            this.advancedSettingsWarningPane1.Size = new System.Drawing.Size(395, 440);
-            this.advancedSettingsWarningPane1.TabIndex = 0;
-            // 
-            // AdvancedSettingsPopup
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 440);
-            this.Controls.Add(this.advancedSettingsWarningPane1);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AdvancedSettingsPopup";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.Text = "Advanced Settings";
-            this.Load += new System.EventHandler(this.AdvancedSettingsPopup_Load);
-            this.ResumeLayout(false);
+      this.advancedSettingsWarningPane1 = new mvCentral.AdvancedSettingsWarningPane();
+      this.SuspendLayout();
+      // 
+      // advancedSettingsWarningPane1
+      // 
+      this.advancedSettingsWarningPane1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.advancedSettingsWarningPane1.Location = new System.Drawing.Point(0, 0);
+      this.advancedSettingsWarningPane1.Name = "advancedSettingsWarningPane1";
+      this.advancedSettingsWarningPane1.Size = new System.Drawing.Size(395, 440);
+      this.advancedSettingsWarningPane1.TabIndex = 0;
+      // 
+      // AdvancedSettingsPopup
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(395, 440);
+      this.Controls.Add(this.advancedSettingsWarningPane1);
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "AdvancedSettingsPopup";
+      this.ShowIcon = false;
+      this.ShowInTaskbar = false;
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+      this.Text = "Advanced Settings";
+      this.Load += new System.EventHandler(this.AdvancedSettingsPopup_Load);
+      this.ResumeLayout(false);
 
         }
 
