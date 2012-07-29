23a2f9f3ec18091894ea7421187719da07fc3297
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2012-07-29 10:42:42 +0000

    Add missing source files

---
+++ trunk/mvCentral/DataProviders/AlbumInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mvCentral.DataProviders
+{
+  class AlbumInfo
+  {
+
+    public string Album { get; set; }
+    public string AlbumArtist { get; set; }
+    public string Artist { get; set; }
+    public string Asin { get; set; }
+    public string Genre { get; set; }
+    public string Image { get; set; }
+    public int Rating { get; set; }
+    public string Review { get; set; }
+    public string Styles { get; set; }
+    public string Tones { get; set; }
+    public string Tracks { get; set; }
+    public int Year { get; set; }
+
+  }
+}
