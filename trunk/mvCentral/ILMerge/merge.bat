d21da75fa0ab6564be20ede8a23ebcb5c5f9fd40
tdnemail@gmail.com <tdnemail@gmail.com@0b9da8b7-768c-aa66-b107-7936e67a8732>
2014-03-02 12:09:08 +0000

    MP 1.6 Build

--- trunk/mvCentral/ILMerge/merge.bat
+++ trunk/mvCentral/ILMerge/merge.bat
@@ -1,4 +1,4 @@
 @echo off
 IF EXIST mvCentral_UNMERGED.dll del mvCentral_UNMERGED.dll
 ren mvCentral.dll mvCentral_UNMERGED.dll
-ilmerge /out:mvCentral.dll mvCentral_UNMERGED.dll nlog.dll cornerstone.dll Lucene.Net.dll
+ilmerge /out:mvCentral.dll mvCentral_UNMERGED.dll nlog.dll cornerstone.dll Lucene.Net.dll /lib:"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /targetplatform:v4
