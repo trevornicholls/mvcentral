e5d350dda33bcc96230ab19e8a5104d49c06aa43
ajs <ajs.blackhole@gmail.com>
2016-06-25 16:47:04 +0300

    New MPE1

--- mvCentral/ILMerge/merge.bat
+++ mvCentral/ILMerge/merge.bat
@@ -1,5 +1,15 @@
 @echo off
-IF EXIST mvCentral_UNMERGED.dll del mvCentral_UNMERGED.dll
-ren mvCentral.dll mvCentral_UNMERGED.dll
-ilmerge /out:mvCentral.dll mvCentral_UNMERGED.dll nlog.dll cornerstone.dll /targetplatform:v4,"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /allowdup
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+    :: 64-bit
+    set PROGS=%programfiles(x86)%
+    goto CONT
+:32BIT
+    set PROGS=%ProgramFiles%
+:CONT
+
+if exist mvCentral_UNMERGED.dll del mvCentral_UNMERGED.dll
+ren mvCentral.dll mvCentral_UNMERGED.dll 
+REM ilmerge.exe /out:mvCentral.dll mvCentral_UNMERGED.dll Nlog.dll cornerstone.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
+ilmerge.exe /out:mvCentral.dll mvCentral_UNMERGED.dll Nlog.dll cornerstone.dll /targetplatform:v4,"%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /allowdup
 del mvCentral_UNMERGED.dll
--- scripts/merge.bat
+++ scripts/merge.bat
@@ -10,4 +10,6 @@ if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 
 if exist mvCentral_UNMERGED.dll del mvCentral_UNMERGED.dll
 ren mvCentral.dll mvCentral_UNMERGED.dll 
-ilmerge.exe /out:mvCentral.dll mvCentral_UNMERGED.dll Nlog.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
+REM ilmerge.exe /out:mvCentral.dll mvCentral_UNMERGED.dll Nlog.dll cornerstone.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
+ilmerge.exe /out:mvCentral.dll mvCentral_UNMERGED.dll Nlog.dll cornerstone.dll /targetplatform:v4,"%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /allowdup
+del mvCentral_UNMERGED.dll
